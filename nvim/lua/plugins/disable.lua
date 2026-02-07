35b01c887b0c786d35d42801dc32a9794fd53214
olefSch <olinho22@gmail.com>
2026-02-07 10:39:34 +0100

    feat(nvim): disable the markdown render
    it is bothering me and I don't want it but wnat to get the basic language support for makrdown which includes it.

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -6,4 +6,7 @@ return {
   { "windwp/nvim-ts-autotag", enabled = false },
 
   { "folke/noice.nvim", enabled = false },
+
+  -- based on markdown plugin
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
